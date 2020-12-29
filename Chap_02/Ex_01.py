3287b9975f72913e05cfaa8c3d61807e650e0fef
Francesco <cicciodb@hotmail.it>
2020-12-29 22:52:59 +0100

    first exercise

---
+++ Chap_02/Ex_01.py
@@ -0,0 +1,12 @@
+import math
+
+# Even or Odd
+
+# Read an integer from user
+num = int(input("Insert a number: "))
+
+# Evaluate if it's even or odd
+if (num % 2) == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
--- workbook_chap_2.py
+++
@@ -1,3 +0,0 @@
-import math
-
-# Even or Odd
